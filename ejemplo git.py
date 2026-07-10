96e00be4a5fa95185f43aa9d789b4991e5f8cc4c
IvoonC <castruitaivoon1730@gmail.com>
2026-07-09 20:19:25 -0600

    Factor 200

--- ejemplo git.py
+++ ejemplo git.py
@@ -5,5 +5,10 @@ b=30
 c=30 * 10 * 5
 print ('resultado=' ,c)
 
+## Cambiado el factor 102 según requerimiento del negocio
+## Cambiado el factor 110 según Junta Control
+## Cambio a factor 200 por Directorio
+
 d=c * 100
 print ('resultado total=', d)
+

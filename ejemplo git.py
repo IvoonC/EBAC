908e81c4ded7f844521d2b1c56e33eed0e7e0225
IvoonC <castruitaivoon1730@gmail.com>
2026-07-23 19:19:08 -0600

    Añadir la variable E 1.1 a la fórmula

--- ejemplo git.py
+++ ejemplo git.py
@@ -1,9 +1,17 @@
 #Prueb de Python
 #Se añadió la varaible "c"
+#Nueva variable E
 a=10
 b=30
-c=30 * 10 * 5
+e=1.1
+c=30 * 10 * 5/e
+
 print ('resultado=' ,c)
 
-d=c * 100
+## Cambiado el factor 102 según requerimiento del negocio
+## Cambiado el factor 110 según Junta Control
+## Cambio a factor 200 por Directorio
+
+d=c * 200
 print ('resultado total=', d)
+
